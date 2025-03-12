c705c7f4e5bc3bd246d3eb4671bdd4dbbc769027
MateusR019 <mateusrogerio777@gmail.com>
2025-03-12 07:50:20 -0300

    fix: ajusta configuração do vite para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: 'src',
   server: {
     port: 1234,
     open: true
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        produtos: '/produtos.html',
+        contato: '/contato.html',
+        sobre: '/sobre.html',
+        carrinho: '/carrinho.html',
+        checkout: '/checkout.html',
+        confirmacao: '/confirmacao.html',
+        conta: '/conta.html',
+        'produto-detalhes': '/produto-detalhes.html'
+      }
+    }
   }
 }) 
